55f96d56f6a229e86c764a72d8fac7bcc91820f1
kq <1798178975@qq.com>
2025-11-28 21:45:30 +0800

    start

---
+++ src/main/java/com/mycompany/computer/network/programming/lab3/ComputerNetworkProgrammingLab3.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.computer.network.programming.lab3;
+
+/**
+ *
+ * @author kq
+ */
+public class ComputerNetworkProgrammingLab3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        // 强制使用 Nimbus 主题（确保所有环境一致）
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("无法设置 Nimbus 主题：" + e.getMessage());
+        }
+
+        java.awt.EventQueue.invokeLater(() -> {
+            new RPClient().setVisible(true);
+        });
+    }
+    
+}
